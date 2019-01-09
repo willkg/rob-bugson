4e0976292758ef159aff1dc633f3fa0fe315fc37
Will Kahn-Greene <willkg@mozilla.com>
2019-01-09 12:18:49 -0500

    Adds list bugs functionality
    This pulls in the functionality of github-list-bugs and adds bug links
    to the pull requests page and the compare page.
    
    Fixes #10

--- github-bugzilla-content.js
+++ github-bugzilla-content.js
@@ -1,17 +1,24 @@
 "use strict";
 
 /**
- * Content script that runs in the context of github web pages, susses out PR
- * page, and adds "attach to bug" links.
+ * Content script that runs in the context of github web pages and:
+ *
+ * 1. susses out PR page and adds "attach to bug" links
+ * 2. linkifies bugzilla bug numbers
  */
 
 // Regexp to match against PR title
 const BUG_RE = /\b(ticket|bug|tracker item|issue)s?:? *([\d ,\+&#and]+)\b/i;
 
 // Base url for attaching a github pr to a bug
-const BASE_URL = 'https://bugzilla.mozilla.org/attachment.cgi?action=enter&bugid=';
+const ATTACH_BASE_URL = 'https://bugzilla.mozilla.org/attachment.cgi?action=enter&bugid=';
 
-const CONTAINER_ID = 'robBugsonAttachLinks';
+// Url for bug lists
+const LIST_BASE_URL = 'https://bugzilla.mozilla.org/buglist.cgi?bug_id=';
+const BUG_BASE_URL = 'https://bugzilla.mozilla.org/show_bug.cgi?id=';
+
+const ATTACH_CONTAINER_ID = 'robBugsonAttachLinks';
+const LIST_CONTAINER_ID = 'robBugsonListLinks';
 
 /**
  * Retrieve the PR number from the pull request page.
@@ -47,6 +54,22 @@ function getBugIds(text) {
     return Array.from(ret);
 }
 
+
+/**
+ * Return array of "bugzilla links"--one for each bug.
+ */
+function getBugLinks(bugIds){
+    return bugIds.map(function(k){
+        let bugLink = document.createElement('a');
+        bugLink.href = BUG_BASE_URL + k;
+        bugLink.target = '_blank';
+        bugLink.className = 'bugzilla_link';
+        bugLink.appendChild(document.createTextNode(k));
+        return bugLink;
+    });
+}
+
+
 /**
  * Return array of "attach links"--one for each bug.
  *
@@ -61,7 +84,7 @@ function getAttachLinks(bugIds, prURL, prNum, prTitle) {
         link.addEventListener("click", (event) => {
             // Send a message to the background script. That handles creating a
             // tab, opening the attach page, and filling in the form.
-            let url = BASE_URL + bugId;
+            let url = ATTACH_BASE_URL + bugId;
             browser.runtime.sendMessage({
                 "attachUrl": url,
                 "prURL": prURL,
@@ -90,6 +113,21 @@ function isPullRequest(url) {
     );
 }
 
+
+/**
+ * Returns true if the URL is a github compare page.
+ *
+ * @param {URL} url
+ * @returns {bool}
+ */
+function isComparePage(url) {
+    return (
+        url.origin == "https://github.com"
+            && url.pathname.split("/")[3] == "compare"
+    );
+}
+
+
 /**
  * Checks if there's already a container and if not, creates one with attach
  * links in it.
@@ -101,11 +139,11 @@ function createAttachLinksContainer() {
     }
 
     // If there's already a link container, then return.
-    var linkContainer = document.getElementById(CONTAINER_ID);
+    var linkContainer = document.getElementById(ATTACH_CONTAINER_ID);
     if (linkContainer == null) {
         // If there's no link container, then we create a new one
         linkContainer = document.createElement('p');
-        linkContainer.id = CONTAINER_ID;
+        linkContainer.id = ATTACH_CONTAINER_ID;
         linkContainer.className = 'subtext';
     }
 
@@ -138,13 +176,77 @@ function createAttachLinksContainer() {
     headerShow.appendChild(linkContainer);
 }
 
-// Run for the current page in case it's a PR
+
+function createBugsList(bugIds){
+    var bugsListContainer = document.getElementById(LIST_CONTAINER_ID);
+    if (bugsListContainer == null) {
+        bugsListContainer = document.createElement('p');
+        bugsListContainer.id = LIST_CONTAINER_ID;
+        bugsListContainer.className = 'subtext';
+    }
+
+    // Remove everything from container so we don't have duplicates
+    while (bugsListContainer.firstChild) {
+        bugsListContainer.removeChild(bugsListContainer.firstChild);
+    }
+    bugsListContainer.appendChild(document.createTextNode('Bugs in commits ('));
+
+    var openAll = document.createElement('a');
+    openAll.href = LIST_BASE_URL + bugIds.join(',');
+    openAll.id = 'open_all_bugzilla_links';
+    openAll.target = '_blank';
+    openAll.appendChild(document.createTextNode('open all'));
+    bugsListContainer.appendChild(openAll);
+    bugsListContainer.appendChild(document.createTextNode('): '));
+
+    var separator = document.createTextNode(', ');
+    getBugLinks(bugIds).forEach(function(bugLink, i){
+        if (i > 0) {
+            bugsListContainer.appendChild(separator.cloneNode(false));
+        }
+        bugsListContainer.appendChild(bugLink);
+    });
+
+    return bugsListContainer;
+}
+
+
+function addBugListToPage() {
+    var url = new URL(window.location.href);
+    var parentElement;
+    var bugIds;
+
+    // If this is a compare page
+    if (isComparePage(url)) {
+        bugIds = [];
+        let elements = document.querySelectorAll('a.message, div.commit-desc pre');
+        Array.prototype.forEach.call(elements, function(el) {
+            bugIds = bugIds.concat(getBugIds(el.textContent));
+        });
+        var insertBeforeEl = document.getElementById('commits_bucket');
+        parentElement = insertBeforeEl.parentElement;
+        parentElement.insertBefore(createBugsList(bugIds), insertBeforeEl);
+        return;
+    }
+
+    if (isPullRequest(url)) {
+        bugIds = getBugIds(getPRTitle());
+        parentElement = document.querySelector('div.gh-header-show');
+        parentElement.appendChild(createBugsList(bugIds));
+    }
+}
+
+
+// Run for the current page
 createAttachLinksContainer();
+addBugListToPage();
+
 
 // Set up an observer to handle pjax loads that might end up on a PR
 let pjaxContainer = document.getElementById('js-repo-pjax-container');
 const pjaxContainerObserver = new window.MutationObserver((mutations) => {
     createAttachLinksContainer();
+    addBugListToPage();
 });
 pjaxContainerObserver.observe(pjaxContainer, {
     childList: true,
