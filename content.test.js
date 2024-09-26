174bcc8f3f761a893ad7db4fc2eb450264ab8e81
Bianca Danforth <biancadanforth@gmail.com>
2024-09-25 22:40:36 -0400

    support Bugzilla merge comment URLs for merge queues
    Fixes #65

--- content.test.js
+++ content.test.js
@@ -1,6 +1,10 @@
 const {
+    addMergeLinks,
+    BUG_BASE_URL,
     getAttachLinks,
     getBugIdsFromPRTitle,
+    MERGE_CONTAINER_ID,
+    PR_STATE_MERGED,
 } = require("./github-bugzilla-content");
 
 describe("Content script", () => {
@@ -48,4 +52,84 @@ describe("Content script", () => {
             },
         );
     });
+
+    describe("addMergeLinks", () => {
+        const pageKind = "pr"
+        const repoInfo = {
+            repoName: "socorro",
+            repoOrg: "mozilla-services"
+        };
+        const prNum = "99"
+        const prTitle = "bug-1: wow impressive title";
+        const prUrl = "https://github.com/mozilla-services/socorro/pull/99"
+        const prState = PR_STATE_MERGED;
+        const bugIds = [1]
+
+        const author = "Scrooge McDuck"
+        const fullCommitSha = "649428ff86e73ef7f6eefb730b8c55e717212d02"
+        const commitSha = fullCommitSha.substring(0, 6);
+        const commitUrl = `/mozilla-services/socorro/commit/${fullCommitSha}`
+
+        const expectedMessage = {
+            "eventName": "mergeComment",
+            "bugUrl": BUG_BASE_URL + bugIds[0],
+            "author": author,
+            "repoOrg": repoInfo.repoOrg,
+            "repoName": repoInfo.repoName,
+            "prNum": prNum,
+            "prUrl": prUrl,
+            "prTitle": prTitle,
+            "authorUrl": "https://github.com/" + author,
+            "commitSha": commitSha,
+            "commitUrl": "https://github.com" + commitUrl
+        };
+
+        it("sends a message to the background script when the merge link is clicked when a PR is merged manually by a user", () => {
+            const sendMessage = jest.spyOn(browser.runtime, "sendMessage").mockResolvedValue({});
+
+            // Mock the HTML structure
+            document.body.innerHTML = `
+                <div class="gh-header-show">
+                    <div id="${MERGE_CONTAINER_ID}">
+                    </div>
+                </div>
+                <div class="TimelineItem">
+                    <div class="TimelineItem-body">
+                        <a class="author" href="/${author}">${author}</a> merged commit <a href="/mozilla-services/socorro/commit/${fullCommitSha}">${commitSha}</a>into main
+                    </div>
+                </div>
+            `;
+            addMergeLinks(pageKind, repoInfo, prNum, prTitle, prUrl, prState, bugIds);
+            const linkContainer = document.getElementById(MERGE_CONTAINER_ID);
+            const mergeLinkEle = linkContainer.querySelector("a.merge_link");
+            mergeLinkEle.click();
+            expect(sendMessage).toHaveBeenCalledWith(expectedMessage);
+        })
+
+        it("sends a message to the background script when the merge link is clicked when a PR is merged via a merge queue", () => {
+            const sendMessage = jest.spyOn(browser.runtime, "sendMessage").mockResolvedValue({});
+            
+            // Mock the HTML structure
+            document.body.innerHTML = `
+                <div class="gh-header-show">
+                    <div id="${MERGE_CONTAINER_ID}">
+                    </div>
+                </div>
+                <div class="TimelineItem">
+                    <div class="TimelineItem-body">
+                        <a class="author" href="/${author}">${author}</a>
+                        added this pull request to the <a href="/mozilla-services/socorro/queue/main" class="Link">merge queue</a>
+                    </div>
+                    <div class="TimelineItem-body">
+                        Merged via the queue into main with commit <a href=${commitUrl}>${commitSha}</a>
+                    </div>
+                </div>
+            `;
+            addMergeLinks(pageKind, repoInfo, prNum, prTitle, prUrl, prState, bugIds);
+            const linkContainer = document.getElementById(MERGE_CONTAINER_ID);
+            const mergeLinkEle = linkContainer.querySelector("a.merge_link");
+            mergeLinkEle.click();
+            expect(sendMessage).toHaveBeenCalledWith(expectedMessage);
+        });
+    })
   });
